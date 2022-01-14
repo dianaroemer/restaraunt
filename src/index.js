9ff5784dee5c244c5d8b7967f55d7b9095209ba0
dominicroemer <dominicroemer@gmail.com>
2022-01-13 17:41:50 -0800

    Initial Build landing page, TODO REMOVE HARD HTML LANDING

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 console.log("Bollocks");
 
 
-import myName from './landing.js';
+// import myName from './landing.js';
+import landing from './landing.js';
 
 
 
-function component() {
-    const element = document.createElement('div');
-    // const btn = 
+// function component() {
+//     const element = document.createElement('div');
+//     // const btn = 
 
-    element.innerHTML = myName('Maroun');
-    element.classList.add('hi');
+//     element.innerHTML = myName('Wonkers');
+//     element.classList.add('hi');
     
-    return element;
-}
+//     return element;
+// }
+// document.body.appendChild(component());
 
-document.body.appendChild(component());
+
+document.body.removeChild(document.getElementById('content'));
+
+document.body.appendChild(landing());
+
+
+
--- src/landing.js
+++ src/landing.js
@@ -1,9 +1,58 @@
-const myName = (name) => {
-    return 'Hi! My name is ' + name + "!";
+
+// Reference module code for creating and exporting a function with a return value for use in another script
+// const myName = (name) => {
+//     console.log("sup");
+//     return 'Hi! My name is ' + name + "!";
+// }
+// export default myName;
+
+
+const landing = () => {
+
+    const landingContainerElement = document.createElement('div');
+
+    const content = document.createElement('div');
+    content.id = 'content';
+    content.style.position = "sticky";
+    content.style.top = "70px";
+    content.style.display = "flex";
+    content.style.justifyContent = "center";
+
+    const boxContainer = document.createElement('div');
+    boxContainer.id = "box";
+    boxContainer.style.textAlign = "center";
+    content.appendChild(boxContainer);
+
+    const logoImage = document.createElement('img');
+    logoImage.classList.add('logo-picture');
+    logoImage.src = './logo.jpg';
+    logoImage.alt = "A blue and white painted saign, saying 'Greek Corner Cafe'";
+    logoImage.style.borderRadius = "50px";
+    boxContainer.appendChild(logoImage);
+
+    const welcome = document.createElement('h2');
+    welcome.classList.add('headLine');
+    welcome.innerHTML = 'Welcome to the Greek Corner Cafe!';
+    boxContainer.appendChild(welcome);
+    
+    const tagline = document.createElement('h3');
+    tagline.classList.add('tagline');
+    tagline.innerHTML = `Man, this shit's the best in town.`;
+    boxContainer.appendChild(tagline);
+
+    const enterButton = document.createElement('button');
+    enterButton.innerText = "Enter";
+    enterButton.style.fontSize = "18px";
+    enterButton.style.marginTop = "3px";
+    enterButton.style.padding = "6px";
+    enterButton.style.borderRadius = "5px";
+    enterButton.style.boxShadow = "0px 2px 6px gray";
+    boxContainer.appendChild(enterButton);
+
+
+    return content;
+
 }
 
-export default myName;
 
-
-
-
+export default landing;
