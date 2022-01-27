de88da976349cb1228e782bdf5e0d3c979308c8d
dominicroemer <dominicroemer@gmail.com>
2022-01-26 16:15:02 -0800

    Add images to menu, add items and basic styling to menu

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,19 @@
 import generateElement from "./utils/helpers";
 
+import Mulligatawny from "./assets/soup-mulligatawny.jpg";
+import BlackBean from "./assets/soup-black-bean.jpg";
+import TomatoRice from "./assets/soup-tomato-rice.jpg";
+
+import Jambalaya from "./assets/soup-jambalaya.jpg";
+import FrenchOnion from "./assets/soup-french-onion.jpg";
+import ClamBisque from "./assets/soup-clam-bisque.jpg";
+
+
 
 const menu = () => {
     const menuContainer = document.createElement('div');
     menuContainer.id = "menu-container";
 
-    // const menuContent = document.createElement('div');
-    // menuContent.classList.add('menu-content');
-    // menuContent.innerHTML = "Menu soup!"
-
-    // menuContainer.appendChild(menuContent);
 
 // Header Signs -----------------------------------------------
 
@@ -51,11 +55,83 @@ const menu = () => {
     menuTitleContent.innerHTML = `MENU`;
     menuTitleContainer.appendChild(menuTitleContent);
 
-    
     menuContainer.appendChild(menuTitleContainer);
 
 
+// Menu Content ----------------------------------------------------------------
 
+    const menuContent = generateElement('div', null, 'menu-content');
+
+    const menuListContainer = generateElement('div', null, 'menu-list-container');
+    menuContent.appendChild(menuListContainer);
+
+    const menuListLeft = generateElement('div', 'menu-list-left', 'menu-list-left');
+    menuListContainer.appendChild(menuListLeft);
+
+    const soupMulligatawny = generateElement('img', 'soup-mulligatawny', 'soup-image');
+    soupMulligatawny.src = Mulligatawny;
+    menuListLeft.appendChild(soupMulligatawny);
+
+    const soupBlackBean = generateElement('img', 'soup-black-bean', 'soup-image');
+    soupBlackBean.src = BlackBean;
+    menuListLeft.appendChild(soupBlackBean);
+
+    const soupTomatoRice = generateElement('img', 'soup-tomato-rice', 'soup-image');
+    soupTomatoRice.src = TomatoRice;
+    menuListLeft.appendChild(soupTomatoRice);
+
+
+    
+    const menuListContent = generateElement('div', null, 'menu-list-content');
+    menuListContainer.appendChild(menuListContent);
+
+    menuListContent.innerHTML = `Hot Soup 
+    <br> <br>
+    Med 16OZ. $2.99 
+    <br> <br>
+    LARGE 32OZ. 3.99 
+    <br> <br>
+    Mulligatawny <br>
+    Crab Bisque <br>
+    Turkey Chili <br>
+    Jambalaya  <br>
+    Black Bean  <br>
+    Chicken Broccoli <br> 
+    Clam Bisque  <br>
+    Split Pea  <br>
+    French Onion  <br>
+    Mushroom Barley  <br>
+    Tomato Rice  <br>
+    <br> 
+    ALL SELECTIONS MADE 
+    <br>
+    FRESH DAILY 
+    <br>  <br>
+     
+    
+    NO SUBSTITUTIONS OR  
+    <br>
+    PHONE ORDERS`;
+
+
+
+    const menuListRight = generateElement('div', 'menu-list-right', 'menu-list-right');
+    menuListContainer.appendChild(menuListRight);
+
+    const soupJambalaya = generateElement('img', 'soup-jambalaya', 'soup-image');
+    soupJambalaya.src = Jambalaya;
+    menuListRight.appendChild(soupJambalaya);
+
+    const soupFrenchOnion = generateElement('img', 'soup-french-onion', 'soup-image');
+    soupFrenchOnion.src = FrenchOnion;
+    menuListRight.appendChild(soupFrenchOnion);
+
+    const soupClamBisque = generateElement('img', 'soup-clam-bisque', 'soup-image');
+    soupClamBisque.src = ClamBisque;
+    menuListRight.appendChild(soupClamBisque);
+
+
+    menuContainer.appendChild(menuContent);
 
 
 
